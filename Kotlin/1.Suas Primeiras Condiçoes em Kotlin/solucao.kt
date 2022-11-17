699b599cb5222f38815db0ed79fd8ade9cc5e279
Luan Simenton <99848672+wbhard@users.noreply.github.com>
2022-11-17 16:18:57 -0300

    Update solucao.kt

--- Kotlin/1.Suas Primeiras Condiçoes em Kotlin/solucao.kt
+++ Kotlin/1.Suas Primeiras Condiçoes em Kotlin/solucao.kt
@@ -2,8 +2,8 @@ fun main() {
    val media = readLine()!!.toDouble();
    
    when {
-       media < 5 -> println("Reprovado");
-       media >= 5 && media < 7 -> println("Recuperacao");
-       media > 7 -> println("Aprovado");
+       media < 5 -> println("REP");
+       media >= 5 && media < 7 -> println("REC");
+       media > 7 -> println("APR");
    }
 } 
